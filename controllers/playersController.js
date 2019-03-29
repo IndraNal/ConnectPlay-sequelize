0eebb8af9a7b8d0589c3bf30a540893435c85c66
Shank <shank@Nomans-MacBook-Pro.local>
2019-03-28 20:44:54 -0500

    map

--- controllers/playersController.js
+++ controllers/playersController.js
@@ -1,20 +1,20 @@
 // Requiring our Player model
-var db = require("../models");
+var db = require('../models');
 
 // geocoder
 var NodeGeocoder = require('node-geocoder');
 
 var options = {
-  provider: "mapquest",
-  apiKey: "sX4NwELW6q0I1WjDtEvnSTLBwIU3o4hl",
-  // Optional depending on the providers
-  httpAdapter: 'https', // Default
-  formatter: null         // 'gpx', 'string', ...
+	provider: 'mapquest',
+	apiKey: 'sX4NwELW6q0I1WjDtEvnSTLBwIU3o4hl',
+	// Optional depending on the providers
+	httpAdapter: 'https', // Default
+	formatter: null // 'gpx', 'string', ...
 };
 
 var geocoder = NodeGeocoder(options);
 // Import the model (player.js) to use its database functions.
-var player = require("../models/player.js");
+var player = require('../models/player.js');
 // //passport Config
 // require("../config/passport.js")(passport);
 
@@ -23,52 +23,50 @@ var player = require("../models/player.js");
 // app.use(passport.session());
 
 module.exports = function(app) {
+	// Create all our routes and set up logic within those routes where required.
 
-// Create all our routes and set up logic within those routes where required.
-app.get("/", function (req, res) {
-  db.Player.findAll({}).then(function(players) {
-    res.render("index", {data: players});
-  });
-});
-app.get("/Registration", function (req, res) {
-  db.Player.findAll({}).then(function(players) {
-    res.render("Registration", {data: players});
-});
-});
-app.get("/home", function (req, res) {
-  db.Player.findAll({}).then(function(players) {
-    res.render("home",{data: players});
-});
-});
+	app.get('/Registration', function(req, res) {
+		db.Player.findAll({}).then(function(players) {
+			res.render('Registration', { data: players });
+		});
+	});
+	app.get('/api/players', function(req, res) {
+		db.Player.findAll({}).then(function(data) {
+			res.json(data);
+		});
+	});
 
-app.post("/api/players", function (req, response) {
-  geocoder.geocode({ address: req.body.Address, country: 'USA', zipcode: req.body.ZipCode }, function (err, res) {
-  console.log(res);
-   }).then(function (res) {
-    console.log(res);
-   
+	app.get('/', function(req, res) {
+		res.render('map');
+	});
 
-    db.Player.create({
-      FirstName :req.body.FirstName,
-      LastName:req.body.LastName,
-      Email:req.body.Email,
-      UserName:req.body.UserName,
-      UserPassword:req.body.UserPassword,
-      Address:req.body.Address,
-      CityName:req.body.CityName,
-      ZipCode:req.body.ZipCode,
-      State:req.body.State,
-      Latitude:res[0].latitude,
-      Longitude:res[0].longitude,
-    })
-  })
-  .then(function(player) {
-    response.json({player});
-  });
+	app.post('/Registration', function(req, response) {
+		geocoder
+			.geocode({ address: req.body.Address, country: 'USA', zipcode: req.body.ZipCode }, function(err, res) {
+				console.log(res);
+			})
+			.then(function(res) {
+				console.log(res);
 
-});
-}
-
+				db.Player.create({
+					FirstName: req.body.FirstName,
+					LastName: req.body.LastName,
+					Email: req.body.Email,
+					UserName: req.body.UserName,
+					UserPassword: req.body.UserPassword,
+					Address: req.body.Address,
+					CityName: req.body.CityName,
+					ZipCode: req.body.ZipCode,
+					State: req.body.State,
+					Latitude: res[0].latitude,
+					Longitude: res[0].longitude
+				});
+			})
+			.then(function(player) {
+				response.json({ player });
+			});
+	});
+};
 
 // app.post('/signup', passport.authenticate('local-signup', {
 //   successRedirect: '/dashboard',
